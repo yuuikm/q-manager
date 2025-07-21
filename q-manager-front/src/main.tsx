898d681efd10751843e4f357884b24c65f5caa91
Mark <morozov-mark@bk.ru>
2025-07-21 23:45:49 +0500

    feat: applied prettier

--- q-manager-front/src/main.tsx
+++ q-manager-front/src/main.tsx
@@ -1,13 +1,13 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 // @ts-ignore
-import "styles/index.css";
-import App from "pages/App.tsx";
-import Header from "pages/layout/Header";
-import Footer from "pages/layout/Footer";
+import 'styles/index.css';
+import App from 'pages/App.tsx';
+import Header from 'pages/layout/Header';
+import Footer from 'pages/layout/Footer';
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <StrictMode>
       <Header />
